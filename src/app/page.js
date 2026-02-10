704a77ac90b2d6962fc7f87bf678e4365650002c
Louise Moraldy <louisemoraldy@proton.me>
2026-02-10 11:23:40 +0100

    add link to form

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl"> Page dacc</h1>
+      <h1 className="text-2xl"> Homepage</h1>
+     <Link className="underline" href="/form">Formulaire </Link>
     </div>
   );
 }
